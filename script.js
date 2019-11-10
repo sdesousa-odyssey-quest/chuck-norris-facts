c3414d76076103313ce07926b39ab910904464e8
sdesousa <sylvaindesousa@free.fr>
2019-11-10 14:52:01 +0100

    script added

---
+++ script.js
@@ -0,0 +1,19 @@
+function fetchChuckNorrisFactJSON() {
+    const url = `https://api.chucknorris.io/jokes/random`;
+    axios.get(url)
+        .then(function(response) {
+            return response.data;
+        })
+        .then(function(fact) {
+            console.log('data decoded from JSON:', fact);
+
+            // Build a block of HTML
+            const factHtml = `
+            <img src="${fact.icon_url}" />
+            <p><strong>${fact.value}</strong></p>
+            `;
+            document.querySelector('#chuck-norris').innerHTML = factHtml;
+        });
+}
+
+fetchChuckNorrisFactJSON();
